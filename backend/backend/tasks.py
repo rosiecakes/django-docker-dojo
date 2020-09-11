54d1433e09b3320c323721d9f81c45fa729ba81f
T Rumbold <thomasrumbold@gmail.com>
2020-09-11 11:43:15 +0100

    Update tasks to make them slightly more readable.

--- backend/backend/tasks.py
+++ backend/backend/tasks.py
@@ -4,11 +4,12 @@ from items.models import Item
 
 @task()
 def update_example(name="update_example"):
-
+    #  Get all Items from the db and count them
     items = Item.objects.all()
-
+    item_count = items.count()
     # For our example, we just increment the price by 2 every run
     for item in items:
         item.price += 2
         item.save()
-        print(item.price)
+    # Print to terminal for demonstration purposes
+    print(f'Price update for {item_count} Items, ran and completed.')
